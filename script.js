9ce5b3610cb8d814a110e8bc909f775a26284d8b
unknown <hailamnguyenthanh@gmail.com>
2020-02-11 18:46:26 +0700

    put anh

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function BoxCloseOpen() {
     if ($(".box").css("display") == "block") {
         $(".box").css("display", "none");
-        $("#open").css("display","block");
+        $("#open").css("display", "block");
     }
     else {
         $(".box").css("display", "block");
-        $("#open").css("display","none");
+        $("#open").css("display", "none");
     }
 
 }
+
+
+
